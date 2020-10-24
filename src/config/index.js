d7548db85b3500d72f838d4dd46ba4c063c5de8e
MinJunSeo <smj0385@naver.com>
2020-10-24 11:15:14 +0900

    Fix: access denied error

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,7 @@ require('dotenv');
 module.exports = {
   SERVER_PORT: process.env.SERVER_PORT,
   DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME
 };
