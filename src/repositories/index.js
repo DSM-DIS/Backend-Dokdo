fa6863b8a708c319e19d3cba1e9565ae1557c7b3
MinJunSeo <smj0385@naver.com>
2020-10-24 10:07:02 +0900

    feat: define repositories relation

---
+++ src/repositories/index.js
@@ -0,0 +1,17 @@
+const { Diary, User } = require('./definitions');
+
+User.hasMany(Diary, {
+  foreignKey: 'author',
+  sourceKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+Diary.belongsTo(User, {
+  foreignKey: 'author',
+  targetKey: 'id',
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Diary };
