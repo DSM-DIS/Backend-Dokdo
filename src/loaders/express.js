87b30d5456c7af9044b0236d9be174eed78af280
MinJunSeo <smj0385@naver.com>
2020-10-24 11:27:54 +0900

    Fix: routing

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('../routes');
 
 module.exports = (app) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use('/', router);
 };
