08ff424991d7d3da59f1e3ff61c0f7bfbd980874
MinJunSeo <smj0385@naver.com>
2020-10-24 09:41:28 +0900

    feat setting configuratino info for developing

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+require('dotenv');
+
+module.exports = {
+  SERVER_PORT: process.env.SERVER_PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME
+};
