ca46aed9e4fe4c34ca2814d190acb4793b1ac36f
MinJunSeo <smj0385@naver.com>
2020-11-18 18:23:25 +0900

    refactor: remove isRegisterdUser util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,7 @@
-const isRegisteredUser = require('./isRegisteredUser');
 const getLastPage = require('./getLastPage');
 const isIntegerArg = require('./isIntegerArg');
 
 module.exports = {
-  isRegisteredUser,
   getLastPage,
   isIntegerArg
 };
