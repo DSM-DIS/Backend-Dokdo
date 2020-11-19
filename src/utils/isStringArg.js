155379454fa0569f4415dd9c17a745a1c0cfc4d7
MinJunSeo <smj0385@naver.com>
2020-11-19 19:06:54 +0900

    pref: remove useless option

--- src/utils/isStringArg.js
+++ src/utils/isStringArg.js
@@ -1,7 +1,7 @@
-function isStringArg(string) {
+const isStringArg = (string) => {
   const isWhiteSpace = /^\s*$/;
 
-  return typeof (string) && string && !isWhiteSpace.test(string);
-}
+  return typeof (string) && !isWhiteSpace.test(string);
+};
 
 module.exports = isStringArg;
