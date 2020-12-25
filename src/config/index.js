345d41640ae2854eac31b8cce9590a4e3d1b132e
MinJunSeo <smj0385@naver.com>
2020-12-26 08:52:02 +0900

    refactor: refactoring cofig/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+import { config } from "dotenv";
+config();
 
-module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT,
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME
+export default {
+  port: Number(process.env.SERVER_PORT),
+  mysql: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME
+  }
 };
