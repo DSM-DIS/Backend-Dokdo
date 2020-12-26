ce0fba92e7c04a73ca818ed7c6ef8a4cc29a5deb
MinJunSeo <smj0385@naver.com>
2020-12-26 12:25:42 +0900

    fix: server port undefined error

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { SERVER_PORT } from "./config";
+import * as config from "./config";
 import { initApp } from "./loaders";
 
 const app = express();
 initApp(app);
 
-app.listen(SERVER_PORT, () => {
+app.listen(config.port, () => {
   console.log(`Server listing at ${SERVER_PORT}`);
 });
