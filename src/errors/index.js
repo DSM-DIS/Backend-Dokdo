a86ecf3a184afe418078fe414c207775254a7fc0
MinJunSeo <smj0385@naver.com>
2020-12-26 10:00:46 +0900

    refactor: error/index.js for distribution code

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,14 +1,6 @@
-class HttpError extends Error {
+export default class HttpError extends Error {
   constructor(status, message) {
     super(message);
     this.status = status;
   }
 }
-
-const BAD_REQUEST = new HttpError(400, 'Invalid parameters supplied');
-const NOT_FOUND = new HttpError(404, 'Not found');
-
-module.exports = {
-  BAD_REQUEST,
-  NOT_FOUND
-};
