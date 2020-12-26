f4e0c79458705c669800ff2763d6466db455adce
MinJunSeo <smj0385@naver.com>
2020-12-26 10:05:18 +0900

    pref: cause property show why error happened

--- src/errors/httpError.js
+++ src/errors/httpError.js
@@ -1,6 +1,7 @@
 export default class HttpError extends Error {
-  constructor(status, message) {
+  constructor(status, message, cause) {
     super(message);
     this.status = status;
+    this.cause = cause;
   }
 }
