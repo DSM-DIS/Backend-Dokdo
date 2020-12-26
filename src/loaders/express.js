50e77eff5a96ebb1d86011548c73e1dd641a3d59
MinJunSeo <smj0385@naver.com>
2020-12-26 10:13:43 +0900

    refactor: express loader

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('../routes');
+import * as express from "express";
+import cors from "cors";
+import router from "../routes";
+import { NotFoundApi } from "../errors";
 
-module.exports = (app) => {
+export default (app) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-
+  
   app.use('/', router);
+  app.use((req, res, next) => {
+    next(NotFoundApi);
+  });
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message
+    });
+  });
 };
