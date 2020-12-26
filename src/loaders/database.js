ccba979a7f984a3749830dce2bfdaa4bd3b03767
MinJunSeo <smj0385@naver.com>
2020-12-26 12:23:08 +0900

    fix: module import twice error

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -1,6 +1,3 @@
-const { Sequelize } = require('sequelize');
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require('../config');
-
 import { Sequelize } from "sequelize";
 import * as config from "../config";
 
