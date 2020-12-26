4ff22e4f56f2ab4a0c9a92111a7b11e5c8c75af3
MinJunSeo <smj0385@naver.com>
2020-12-26 10:12:15 +0900

    feat: NotFoundApi http error

--- src/errors/index.js
+++ src/errors/index.js
@@ -3,5 +3,6 @@ import HttpError from "./httpError";
 export const BadRequest = new HttpError(400, "Bad Request");
 export const ForbiddenDiaryBook = new HttpError(403, "Forbidden", "diary book");
 export const ForbiddenPage = new HttpError(403, "Forbidden", "page");
+export const NotFoundApi = new HttpError(404, "Not Found");
 export const NotFoundDiaryBook = new HttpError(404, "Not Found", "diary book");
 export const NotFoundPage = new HttpError(404, "Not Found", "page");
