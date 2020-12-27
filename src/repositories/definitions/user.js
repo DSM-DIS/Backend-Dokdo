73b8d25682d4ff9bae8a7eea0da365def2871f23
MinJunSeo <smj0385@naver.com>
2020-12-27 21:42:03 +0900

    fix: node syntax

--- src/repositories/definitions/user.js
+++ src/repositories/definitions/user.js
@@ -1,5 +1,5 @@
-import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../../loaders/database";
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../../loaders/database');
 
 class User extends Model {}
 
@@ -21,4 +21,4 @@ User.init({
   tableName: 'user'
 });
 
-export { User };
+module.exports = User;
