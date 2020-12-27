1d5c85b8013c61542e6f19e44ca2e052faf441b6
MinJunSeo <smj0385@naver.com>
2020-12-27 21:35:41 +0900

    pref: final error handler

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,21 +1,31 @@
-import * as express from "express";
-import cors from "cors";
-import router from "../routes";
-import { NotFoundApi } from "../errors";
+const express = require('express');
+const cors = require('cors');
+const router = require('');
+const { NotFoundApi, InternalServer } = require('../errors');
 
-export default (app) => {
+const loadExpressApp = (app) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  
+
   app.use('/', router);
   app.use((req, res, next) => {
     next(NotFoundApi);
   });
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message
-    });
+    if (err.status && err.status !== 500) {
+      res.status(err.status);
+      res.json({
+        message: err.message
+      });
+    } else {
+      console.error(err);
+      res.status(InternalServer.status);
+      res.json({
+        message: InternalServer.message
+      });
+    }
   });
 };
+
+module.exports = loadExpressApp;
