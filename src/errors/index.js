e69534bb065d390b6d1b3fd3e40bcb1f0c42085b
MinJunSeo <smj0385@naver.com>
2020-12-27 21:27:35 +0900

    fix: node syntax

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,8 +1,19 @@
-import HttpError from "./httpError";
+const HttpError = require('./httpError');
 
-export const BadRequest = new HttpError(400, "Bad Request");
-export const ForbiddenDiaryBook = new HttpError(403, "Forbidden", "diary book");
-export const ForbiddenPage = new HttpError(403, "Forbidden", "page");
-export const NotFoundApi = new HttpError(404, "Not Found");
-export const NotFoundDiaryBook = new HttpError(404, "Not Found", "diary book");
-export const NotFoundPage = new HttpError(404, "Not Found", "page");
+const BadRequest = new HttpError(400, "Bad Request");
+const ForbiddenDiaryBook = new HttpError(403, "Forbidden", "diary book");
+const ForbiddenPage = new HttpError(403, "Forbidden", "page");
+const NotFoundApi = new HttpError(404, "Not Found");
+const NotFoundDiaryBook = new HttpError(404, "Not Found", "diary book");
+const NotFoundPage = new HttpError(404, "Not Found", "page");
+const InternalServer = new HttpError(500, 'Internal Server');
+
+module.exports = {
+  BadRequest,
+  ForbiddenDiaryBook,
+  ForbiddenPage,
+  NotFoundApi,
+  NotFoundDiaryBook,
+  NotFoundPage,
+  InternalServer
+};
