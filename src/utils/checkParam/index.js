921e52ffe377fdde1172b656513a8f107dd89d60
MinJunSeo <smj0385@naver.com>
2020-12-28 19:56:04 +0900

    pref: integrate check diary book utils

---
+++ src/utils/checkParam/index.js
@@ -0,0 +1,11 @@
+const checkIntegerArg = require('./checkIntegerArg');
+const checkCreatedDiaryBook = require('./checkCreatedDiaryBook');
+const checkYourDiaryBook = require('./checkYourDiaryBook');
+const checkDiaryBookId = require('./checkDiaryBookId');
+
+module.exports = {
+  checkIntegerArg,
+  checkCreatedDiaryBook,
+  checkYourDiaryBook,
+  checkDiaryBookId
+};
