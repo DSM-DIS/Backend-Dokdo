ee98e43c133d01c353c0bec6677daee516cb5a17
MinJunSeo <smj0385@naver.com>
2020-12-28 20:00:35 +0900

    feat: check page paramter

--- src/utils/checkParam/index.js
+++ src/utils/checkParam/index.js
@@ -2,10 +2,12 @@ const checkIntegerArg = require('./checkIntegerArg');
 const checkCreatedDiaryBook = require('./checkCreatedDiaryBook');
 const checkYourDiaryBook = require('./checkYourDiaryBook');
 const checkDiaryBookId = require('./checkDiaryBookId');
+const checkPage = require('./checkPage');
 
 module.exports = {
   checkIntegerArg,
   checkCreatedDiaryBook,
   checkYourDiaryBook,
-  checkDiaryBookId
+  checkDiaryBookId,
+  checkPage
 };
