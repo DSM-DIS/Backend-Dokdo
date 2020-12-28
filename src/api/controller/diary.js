113f231e21aab30cb34854722b1272c361159145
MinJunSeo <smj0385@naver.com>
2020-12-28 20:03:14 +0900

    feat: get diary content controller

---
+++ src/api/controller/diary.js
@@ -0,0 +1,24 @@
+const { Diary } = require('../../repositories');
+const DiaryService = require('../../services/diary');
+const diaryService = new DiaryService();
+const { BadRequest } = require('../../errors');
+const { checkDiaryBookId, checkPage } = require('../../utils');
+
+const getDiary = async (req, res, next) => {
+  try {
+    const userId = req.headers.userId;
+    const diaryBookId = parseInt(req.params.id);
+    const page = parseInt(req.params.page);
+
+    // error handler
+    await checkDiaryBookId(diaryBookId);
+    await checkPage(page);
+
+    const data = await diaryService.readingDiary(diaryBookId, page);
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getDiary;
