1185092041401fa79053c972765de86ddd0dcac4
MinJunSeo <smj0385@naver.com>
2020-12-28 20:19:38 +0900

    feat: writing diary controller

--- src/api/controller/diary.js
+++ src/api/controller/diary.js
@@ -2,7 +2,7 @@ const { Diary } = require('../../repositories');
 const DiaryService = require('../../services/diary');
 const diaryService = new DiaryService();
 const { BadRequest } = require('../../errors');
-const { checkDiaryBookId, checkPage } = require('../../utils');
+const { checkDiaryBookId, checkOwner, checkPage, checkContent } = require('../../utils');
 
 const getDiary = async (req, res, next) => {
   try {
@@ -21,13 +21,22 @@ const getDiary = async (req, res, next) => {
   }
 };
 
-// const writingDiary = async (req, res, next) => {
-//   try {
-//     const userId = 
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const writingDiary = async (req, res, next) => {
+  try {
+    const userId = req.headers.userId;
+    const diaryBookId = parseInt(req.params.id);
+    const { content } = req.body;
+
+    // error handler
+    await checkOwner(userId, diaryBookId);
+    checkContent(content);
+
+    await diaryService.writingDiary(userId, diaryBookId, content);
+    res.status(201).send();
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getDiary
