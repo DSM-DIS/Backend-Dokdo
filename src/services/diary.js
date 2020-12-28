86c5f315ca297b57838e8439badf43fa33b6c8e3
MinJunSeo <smj0385@naver.com>
2020-12-28 20:22:07 +0900

    pref: controller do not check error

--- src/services/diary.js
+++ src/services/diary.js
@@ -1,23 +1,20 @@
-const { stringLen } = require('../config');
 const { Diary } = require('../repositories');
+const { checkDiaryBookId, checkOwner, checkPage, checkContent } = require('../../utils');
 
 class DiaryService {
   constructor() {
     this.diaryModel = Diary;
   }
 
-  async readingDiary(diaryBookId, page) {
+  async readingDiary(userId, diaryBookId, page) {
+    // error handler
+    await checkDiaryBookId(userId, diaryBookId);
+    await checkPage(page);
+
     return await this.diaryModel.readingDiary(diaryBookId, page);
   }
 
   async writingDiary(userId, diaryBookId, page, content) {
-    if (!isIntegerArg(diaryBookId) || !isIntegerArg(page)) {
-      throw BAD_REQUEST;
-    }
-    if (!isStringArg(content) || content.length > CONTENT_LEN) {
-      throw BAD_REQUEST;
-    }
-
     await this.diaryModel.writingDiary(diaryBookId, userId, content, page);
   }
 }
