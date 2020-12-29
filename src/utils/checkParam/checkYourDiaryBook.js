4e0d5058db89a5524c02c34232cf89bb0689390f
MinJunSeo <smj0385@naver.com>
2020-12-29 15:29:05 +0900

    feat: axios setting

--- src/utils/checkParam/checkYourDiaryBook.js
+++ src/utils/checkParam/checkYourDiaryBook.js
@@ -1,11 +1,15 @@
-const axios = require('axios').default;
+const Axios = require('axios').default;
+const { baseURL } = require('../../config');
 const { ForbiddenDiaryBook } = require('../../errors');
+
+const axios = Axios.create({
+  baseURL: baseURL.gyeongsang,
+  timeout: 1000
+});
 
 const checkYourDiary = async (userId, diaryBookId) => {
   const res = await axios.get('/repositories/diary-book', {
-    headers: {
-      id: userId
-    }
+    headers: { id: userId }
   });
 
   for (const diaryBook of res.diaryBooks) {
