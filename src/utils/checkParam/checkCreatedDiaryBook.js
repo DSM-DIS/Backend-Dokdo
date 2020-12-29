8b979dfe1145c20a3632d1da18e9c30b892e006f
MinJunSeo <smj0385@naver.com>
2020-12-29 15:26:05 +0900

    feat: axios setting

--- src/utils/checkParam/checkCreatedDiaryBook.js
+++ src/utils/checkParam/checkCreatedDiaryBook.js
@@ -1,5 +1,11 @@
-const axios = require('axios').default;
+const Axios = require('axios').default;
+const { baseURL } = require('../../config');
 const { NotFoundDiaryBook } = require('../../errors');
+
+const axios = Axios.create({
+  baseURL: baseURL.jeonju,
+  timeout: 1000
+});
 
 const checkCreatedDiaryBook = async (diaryBookId) => {
   const res = await axios.get(`/repositories/diary-books/${diaryBookId}`);
